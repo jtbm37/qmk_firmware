379fd2eb2dd724f7bd6f1e18fc377fd0a97017e1
jtbm37 <jtbm37@github.com>
2021-09-23 02:00:49 +0100

    more stuff

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dvorak/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dvorak/keymap.c
@@ -19,19 +19,21 @@ extern keymap_config_t keymap_config;
 
 #define RAISE_E LT(_RAISE,KC_E)
 #define RAISE_T LT(_RAISE,KC_T)
+#define RAISE_SPC LT(_RAISE,KC_SPC)
 
 #define PRINT_SCREEN LGUI(KC_PSCREEN)
 #define CTL_ESC LCTL_T(KC_ESC)
 #define ALT_BSPC LALT_T(KC_BSPC)
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT_5x6(
      KC_DLR , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EXLM,
 		KC_ESC , KC_SCLN  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,                  KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_SLASH,
-     KC_LSFT, KC_A  , KC_O  , RAISE_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , RAISE_T  , KC_N  ,KC_S, KC_MINS,
+     KC_LSFT, KC_A  , KC_O  , RAISE_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , KC_T  , KC_N  ,KC_S, KC_MINS,
      DEV, KC_QUOTE  , KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_DEL,
 														KC_NUBS,KC_GRV,                                                       KC_NUHS, S(KC_QUOT),
-																				ALT_BSPC,CTL_ESC,                        KC_LSFT, KC_SPC,
+																				ALT_BSPC,CTL_ESC,                        KC_LSFT, RAISE_SPC,
 																				PRINT_SCREEN,SYMS,                   KC_LGUI, SYMS,  
 																				KC_TAB, KC_ENT,                         KC_HOME,KC_END
   ),
@@ -41,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,KC_E,_______,_______,                        _______,_______,_______,_______,_______,_______,
      DVK,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
                                              _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+                                             KC_LSFT,KC_LCTL,            _______,_______,
+                                             _______,KC_SPC,            _______,_______,
+                                             _______,KC_ENT,            _______,_______
 ),
 
 	/* not used because it causes shift to be too slow */
