b7e29c613ea1b7d3a40b9c7c06db9beaa10b9269
jtbm37 <jtbm37@github.com>
2022-01-07 17:40:24 +0000

    WIP

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dvorak/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dvorak/keymap.c
@@ -17,7 +17,7 @@ extern keymap_config_t keymap_config;
 #define DEV MO(_DEV)
 #define DVK DF(_DVORAK)
 #define GIG DF(_GAMING1)
-#define GIG2 DF(_GAMING2)
+#define GIG2 MO(_GAMING2)
 #define SHIFTED MO(_SHIFTED)
 
 #define RAISE_E LT(_RAISE,KC_E)
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DEV, KC_QUOTE  , KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_DEL,
 														KC_NUBS,KC_GRV,                                                       KC_NUHS, S(KC_QUOT),
 																				ALT_BSPC,CTL_ESC,                        SHIFTED, RAISE_SPC,
-																				PRINT_SCREEN,OSX,                   KC_LGUI, OSX,  
+																				PRINT_SCREEN,/*OSX*/KC_ENT,                   KC_LGUI, OSX,  
 																				KC_TAB, KC_ENT,                         KC_HOME,KC_END
   ),
   [_SHIFTED] = LAYOUT_5x6(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		S(KC_TAB), S(KC_SCLN)  , S(KC_COMM)  , S(KC_DOT)  , S(KC_P)  , S(KC_Y)  ,          S(KC_F)  , S(KC_G)  , S(KC_C)  , S(KC_R)  , S(KC_L)  ,S(KC_SLASH),
     _______, S(KC_A)  , S(KC_O)  , S(KC_E)  , S(KC_U)  , S(KC_I)  ,              S(KC_D)  , S(KC_H)  , S(KC_T)  , S(KC_N)  ,S(KC_S), S(KC_MINS),
     _______, S(KC_2)  , S(KC_Q)  , S(KC_J)  , S(KC_K)  , S(KC_X)  ,             S(KC_B)  , S(KC_M)  ,S(KC_W),S(KC_V) ,S(KC_Z) ,_______,
-		S(KC_NUBS),S(KC_GRV),                                                       KC_NUHS, S(KC_QUOT),
+		S(KC_NUBS),S(KC_GRV),                                                       S(KC_6), S(KC_QUOT),
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______
@@ -83,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_GAMING1] = LAYOUT_5x6(
 		KC_ESC,KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         DVK,DVK,DVK,DVK,DVK,DVK,
-     KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                          _______,  _______,  _______,  _______,  _______,  _______,  
-     KC_LALT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                          _______,  _______,  _______,  _______,  _______,  _______,  
+     KC_TAB,  KC_I,  KC_Q,  KC_W,  KC_E,  KC_R,                          _______,  _______,  _______,  _______,  _______,  _______,  
+     KC_LALT,  KC_M,  KC_A,  KC_S,  KC_D,  KC_F,                          _______,  _______,  _______,  _______,  _______,  _______,  
      XXXXXXX,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                          _______,  _______,  _______,  _______,  _______,  _______,  
                                              _______,_______,            _______,_______,
                                              KC_LSFT,KC_LCTL,            _______,_______,
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                              KC_LALT,KC_ENT,            _______,_______
 ),
   [_GAMING2] = LAYOUT_5x6(
-		KC_ESC,KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,                         _______,_______,_______,_______,_______,_______,
-     XXXXXXX,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,                          _______,  _______,  _______,  _______,  _______,  _______,  
-     XXXXXXX,  KC_H,  KC_J,  KC_K,  KC_L,  KC_DOT,                          _______,  _______,  _______,  _______,  _______,  _______,  
+		KC_ESC,KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,                         DVK,DVK,DVK,DVK,DVK,DVK,
+     XXXXXXX,  KC_Y,  KC_U,  KC_G,  KC_O,  KC_P,                          _______,  _______,  _______,  _______,  _______,  _______,  
+     XXXXXXX,  KC_H,  KC_J,  KC_K,  KC_T,  KC_DOT,                          _______,  _______,  _______,  _______,  _______,  _______,  
      XXXXXXX,  KC_N,  KC_M,  KC_L,  KC_ENT,  KC_ESC,                          _______,  _______,  _______,  _______,  _______,  _______,  
                                              _______,_______,            _______,_______,
                                              KC_LSFT,KC_LCTL,            _______,_______,
